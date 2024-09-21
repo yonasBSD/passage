b603af205c2204cb0aa31fedb54bd3505bc4c33d
Yonas <yonas.y@gmail.com>
2024-09-21 14:52:57 -0400

    Remove clipboard.

--- src/cmd/show.rs
+++ src/cmd/show.rs
@@ -1,19 +1,9 @@
-use clipboard::{ClipboardContext, ClipboardProvider};
-
 use crate::error::Error;
 use crate::store::PasswordStore;
 
 pub fn show(store: PasswordStore, item: &str, copy_to_clipboard: bool) -> Result<(), Error> {
     let secret = store.get(item)?;
 
-    if copy_to_clipboard {
-        let first_line = secret.split('\n').next().unwrap_or("");
-        let mut ctx: ClipboardContext = ClipboardProvider::new()?;
-        ctx.set_contents(first_line.to_string())?;
-        eprintln!("Copied password for {} to clipboard.", item);
-    } else {
-        println!("{}", secret);
-    }
-
+    println!("{}", secret);
     Ok(())
 }
